e6614e3ad3fe75025ff126bef41a1686df3eec09
xplorer5 <73779048+xplorer5@users.noreply.github.com>
2024-06-21 23:08:24 +0530

    Created

--- WinFormsApp/ViewSubmissionForm.vb
+++ WinFormsApp/ViewSubmissionForm.vb
@@ -1,5 +1,22 @@
-﻿Public Class ViewSubmissionForm
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+Imports HotKeySet
+Public Class ViewSubmissionForm
+Private Sub ViewSubmissionsButton_Click(sender As Object, e As EventArgs) Handles ViewSubmissionsButton.Click
+    ' Simulate data retrieval (replace with your actual data source logic)
+    Dim name As String = "John Doe"
+    Dim email As String = "johndoe@gmail.com"
+    Dim phoneNumber As String = "1234567890"
+    Dim gitHubLink As String = "https://github.com/john_doe/my_slidely_t"
 
-    End Sub
+    ' Set the retrieved data to the TextBoxes
+    nameTextBox.Text = name
+    emailTextBox.Text = email
+    phoneNumberTextBox.Text = phoneNumber
+    gitHubLinkTextBox.Text = gitHubLink
+End Sub
+
+Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    ' Register keyboard shortcut for View Submissions button (Ctrl + V)
+    HotKeySet.RegisterHotKey(Me, 1, HotKeyModifiers.Control, Keys.V)
+End Sub
+
 End Class
