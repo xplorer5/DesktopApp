5be774b72fc53fe82d7063dc5fc422d8035c4207
xplorer5 <73779048+xplorer5@users.noreply.github.com>
2024-06-21 23:06:26 +0530

    Created

--- WinFormsApp/CreateNewSubmission.vb
+++ WinFormsApp/CreateNewSubmission.vb
@@ -1,9 +1,16 @@
-﻿Public Class CreateNewSubmission
+Imports HotKeySet
+Public Class CreateNewSubmission
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Clear any existing data in view TextBoxes (if applicable)
+    nameTextBox.Text = ""
+    emailTextBox.Text = ""
+    phoneNumberTextBox.Text = ""
+    gitHubLinkTextBox.Text = ""
 
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
+         ' Register keyboard shortcut for Create New Submission button (Ctrl + N)
+    HotKeySet.RegisterHotKey(Me, 2, HotKeyModifiers.Control, Keys.N)
     End Sub
 End Class
